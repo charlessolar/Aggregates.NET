e293ee653c13f8797ff8d7a09ad7500758d201ff
Charles Solar <charlessolar@gmail.com>
2015-05-10 01:29:43 -0500

    Bumping package version

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyProduct("Aggregates.NET")]
 [assembly: AssemblyDescription(".NET event sourced domain driven design model via NServicebus and EventStore")]
 [assembly: AssemblyCopyright("Copyright © Charles Solar 2015")]
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
