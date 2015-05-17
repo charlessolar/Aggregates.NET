27e7db9ca2dd0acaec798da67e3734cb76bff644
Charles Solar <charlessolar@gmail.com>
2015-05-16 23:09:02 -0500

    NSB dispatcher will use the IMessageHandlerRepository to delivery events directly to event handlers in the assembly mapping for the project instead of publishing them to unknown subscribers. This will mean the event will be processed inline when the event is received from event store, whereas with publish the event is queued in MSMQ and executed "later" Because we are recording stream position immediately after dispatching the event, I felt it would be a bad idea to record an event position for events that have technically not been processed.  Even though NSB promises the event will eventually be processed, this is more of a pedantic change

--- src/Aggregates.NET.Consumer/NServicebusDispatcher.cs
+++ src/Aggregates.NET.Consumer/NServicebusDispatcher.cs
@@ -5,23 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using NServiceBus;
+using NServiceBus.ObjectBuilder;
+using NServiceBus.Unicast;
 
 namespace Aggregates
 {
     public class NServiceBusDispatcher : IDispatcher
     {
         private readonly IBus _bus;
+        private readonly IMessageHandlerRegistry _handlerRegistry;
+        private readonly IBuilder _builder;
 
-        public NServiceBusDispatcher(IBus bus)
+        public NServiceBusDispatcher(IBus bus, IBuilder builder)
         {
             _bus = bus;
+            _builder = builder;
+            _handlerRegistry = builder.Build<IMessageHandlerRegistry>();
         }
 
         public void Dispatch(Object @event)
         {
             // We can't publish unstructured POCOs
             if (@event is JObject) return;
-            _bus.Publish(@event);
+
+            // Use NSB internal handler registry to directly call Handle(@event)
+            // This will prevent the event from being queued on MSMQ
+            var handlersToInvoke = _handlerRegistry.GetHandlerTypes(@event.GetType()).ToList();
+
+            foreach (var handlerType in handlersToInvoke)
+            {
+                var handler = _builder.Build(handlerType);
+                _handlerRegistry.InvokeHandle(handler, @event);
+            }
+                //_bus.Publish(@event);
         }
     }
 }
