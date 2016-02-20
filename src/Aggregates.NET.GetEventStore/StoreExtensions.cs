8811a9ea0f657def2e5035b39690cbfb5d0fdc13
Charles Solar <charlessolar@gmail.com>
2016-02-19 23:40:05 -0600

    Removing the GetEventStore depend for the consumer so it doesn't need to install Domain as well

--- src/Aggregates.NET.GetEventStore/StoreExtensions.cs
+++ src/Aggregates.NET.GetEventStore/StoreExtensions.cs
@@ -12,57 +12,10 @@ namespace Aggregates.Extensions
 {
     public static class StoreExtensions
     {
-        public static byte[] AsByteArray(this String json)
-        {
-            return Encoding.UTF8.GetBytes(json);
-        }
 
         public static String Serialize(this ISnapshot snapshot, JsonSerializerSettings settings)
         {
             return JsonConvert.SerializeObject(snapshot, settings);
-        }
-        public static String Serialize(this Object @event, JsonSerializerSettings settings)
-        {
-            return JsonConvert.SerializeObject(@event, settings);
-        }
-
-        public static String Serialize(this EventDescriptor descriptor, JsonSerializerSettings settings)
-        {
-            return JsonConvert.SerializeObject(descriptor, settings);
-        }
-
-        public static Object Deserialize(this byte[] bytes, string type, JsonSerializerSettings settings)
-        {
-            var json = Encoding.UTF8.GetString(bytes);
-            var resolved = Type.GetType(type, false);
-            if (resolved == null) return null;
-
-            return JsonConvert.DeserializeObject(json, resolved, settings);
-        }
-
-        public static EventDescriptor Deserialize(this byte[] bytes, JsonSerializerSettings settings)
-        {
-            var json = Encoding.UTF8.GetString(bytes);
-            return JsonConvert.DeserializeObject<EventDescriptor>(json, settings);
-        }
-
-        public static T Deserialize<T>(this byte[] bytes, JsonSerializerSettings settings)
-        {
-            var json = Encoding.UTF8.GetString(bytes);
-            return JsonConvert.DeserializeObject<T>(json);
-        }
-
-        public static String toLowerCamelCase(this String type)
-        {
-            // Unsure if I want to trim the namespaces or not
-            var name = type.Substring(type.LastIndexOf('.') + 1);
-            return char.ToLower(name[0]) + name.Substring(1);
-        }
-
-        public static T WaitForResult<T>(this Task<T> task)
-        {
-            task.Wait();
-            return task.Result;
         }
 
         public static void DispatchEvents(this IEventStoreConnection client, IDispatcher dispatcher, JsonSerializerSettings settings)
---
+++ src/Aggregates.NET/Extensions/StoreExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Aggregates.Contracts;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregates.Extensions
+{
+    public static class StoreExtensions
+    {
+        public static byte[] AsByteArray(this String json)
+        {
+            return Encoding.UTF8.GetBytes(json);
+        }
+
+        public static String Serialize(this Object @event, JsonSerializerSettings settings)
+        {
+            return JsonConvert.SerializeObject(@event, settings);
+        }
+
+        public static String Serialize(this IEventDescriptor descriptor, JsonSerializerSettings settings)
+        {
+            return JsonConvert.SerializeObject(descriptor, settings);
+        }
+
+        public static Object Deserialize(this byte[] bytes, string type, JsonSerializerSettings settings)
+        {
+            var json = Encoding.UTF8.GetString(bytes);
+            var resolved = Type.GetType(type, false);
+            if (resolved == null) return null;
+
+            return JsonConvert.DeserializeObject(json, resolved, settings);
+        }
+
+        public static IEventDescriptor Deserialize(this byte[] bytes, JsonSerializerSettings settings)
+        {
+            var json = Encoding.UTF8.GetString(bytes);
+            return JsonConvert.DeserializeObject<IEventDescriptor>(json, settings);
+        }
+
+        public static T Deserialize<T>(this byte[] bytes, JsonSerializerSettings settings)
+        {
+            var json = Encoding.UTF8.GetString(bytes);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public static String toLowerCamelCase(this String type)
+        {
+            // Unsure if I want to trim the namespaces or not
+            var name = type.Substring(type.LastIndexOf('.') + 1);
+            return char.ToLower(name[0]) + name.Substring(1);
+        }
+
+        public static T WaitForResult<T>(this Task<T> task)
+        {
+            task.Wait();
+            return task.Result;
+        }
+    }
+}
