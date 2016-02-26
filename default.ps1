ea73b8c729bad81e18035638f56312dd5c9d39c1
Charles Solar <charlessolar@gmail.com>
2016-02-25 18:49:49 -0600

    Attempting to get the symbols published to MS - might end badly

--- default.ps1
+++ default.ps1
@@ -68,5 +68,6 @@ task CreateNuGetPackages -depends ILMerge {
 	$versionString = Get-Version $assemblyInfoFilePath
 	gci $srcDir -Recurse -Include *.nuspec | % {
 		exec { .$rootDir\nuget\nuget.exe pack $_ -o $buildOutputDir -properties "version=$versionString;configuration=$configuration" }
+		exec { .$rootDir\nuget\nuget.exe pack $_ -symbols -o $buildOutputDir -properties "version=$versionString;configuration=$configuration" }
 	}
 }
