17796dc1e2a0fd1582531dac2f07d3c6d589a60a
Charles Solar <charlessolar@gmail.com>
2016-04-14 20:48:17 -0500

    Add more IHandleContext extension methods

--- src/Aggregates.NET/Extensions/BusExtensions.cs
+++ src/Aggregates.NET/Extensions/BusExtensions.cs
@@ -22,5 +22,29 @@ namespace Aggregates.Extensions
             var incoming = context.Context.PhysicalMessage;
             context.Bus.Send(incoming.ReplyToAddress, String.IsNullOrEmpty(incoming.CorrelationId) ? incoming.Id : incoming.CorrelationId, message);
         }
+        public static void PublishAsync<T>(this IHandleContext context, Action<T> message)
+        {
+            context.Bus.Publish<T>(message);
+        }
+        public static void PublishAsync(this IHandleContext context, object message)
+        {
+            context.Bus.Publish(message);
+        }
+        public static void SendAsync<T>(this IHandleContext context, Action<T> message)
+        {
+            context.Bus.Send(message);
+        }
+        public static void SendAsync(this IHandleContext context, object message)
+        {
+            context.Bus.Send(message);
+        }
+        public static void SendAsync<T>(this IHandleContext context, String destination, Action<T> message)
+        {
+            context.Bus.Send(destination, message);
+        }
+        public static void SendAsync(this IHandleContext context, String destination, object message)
+        {
+            context.Bus.Send(destination, message);
+        }
     }
 }
