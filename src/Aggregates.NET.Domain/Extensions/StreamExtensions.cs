b54ab604d5cb447a5914db1dcaca46e24e167fcf
Charles Solar <charlessolar@gmail.com>
2016-08-30 16:29:01 -0500

    Bumping version is a lot more work than I initially thought

--- src/Aggregates.NET.Domain/Extensions/StreamExtensions.cs
+++ src/Aggregates.NET.Domain/Extensions/StreamExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Aggregates.Contracts;
+using NServiceBus;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace Aggregates.Extensions
 {
     public static class StreamExtensions
     {
-        public static IEnumerable<T> AllEvents<T>(this IEventStream stream, Boolean? backwards)
+        public static IEnumerable<T> AllEvents<T>(this IEventStream stream, Boolean? backwards) where T : IEvent
         {
             foreach( var @event in stream.AllEvents(backwards))
             {
@@ -17,7 +18,7 @@ namespace Aggregates.Extensions
                     yield return (T)@event.Event;
             }
         }
-        public static IEnumerable<T> OOBEvents<T>(this IEventStream stream, Boolean? backwards)
+        public static IEnumerable<T> OOBEvents<T>(this IEventStream stream, Boolean? backwards) where T : IEvent
         {
             foreach (var @event in stream.OOBEvents(backwards))
             {
