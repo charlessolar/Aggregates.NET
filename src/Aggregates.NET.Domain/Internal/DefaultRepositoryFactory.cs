c86770c1a623635b8b266bb8d3a365f2c0994dca
Charles Solar <charlessolar@gmail.com>
2017-07-25 01:04:58 -0500

    Make the delayed factory repo cache a concurrent dictionary, dict is not thread safe

--- src/Aggregates.NET.Domain/Internal/DefaultRepositoryFactory.cs
+++ src/Aggregates.NET.Domain/Internal/DefaultRepositoryFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using Aggregates.Contracts;
 using NServiceBus.ObjectBuilder;
@@ -7,39 +8,30 @@ namespace Aggregates.Internal
 {
     class DefaultRepositoryFactory : IRepositoryFactory
     {
-        private static readonly IDictionary<Type, Type> RepoCache = new Dictionary<Type, Type>();
+        private static readonly ConcurrentDictionary<Type, Type> RepoCache = new ConcurrentDictionary<Type, Type>();
 
         public IRepository<T> ForAggregate<T>(IBuilder builder) where T : Aggregate<T>
         {
-            Type repoType;
-            if (!RepoCache.TryGetValue(typeof(T), out repoType))
-                repoType = RepoCache[typeof(T)] = typeof(Repository<>).MakeGenericType(typeof(T));
+            var repoType = RepoCache.GetOrAdd(typeof(T), (key) => typeof(Repository<>).MakeGenericType(typeof(T)));
 
             return (IRepository<T>)Activator.CreateInstance(repoType, builder);
         }
         public IRepository<TParent, T> ForEntity<TParent, T>(TParent parent, IBuilder builder) where T : Entity<T, TParent> where TParent : Entity<TParent>
         {
-            // Is it possible to have an entity type with multiple different types of parents?  Nope
-            Type repoType;
-            if (!RepoCache.TryGetValue(typeof(T), out repoType))
-                repoType = RepoCache[typeof(T)] = typeof(Repository<,>).MakeGenericType(typeof(TParent), typeof(T));
+            var repoType = RepoCache.GetOrAdd(typeof(T), (key) => typeof(Repository<,>).MakeGenericType(typeof(TParent), typeof(T)));
 
             return (IRepository<TParent, T>)Activator.CreateInstance(repoType, parent, builder);
             
         }
         public IPocoRepository<T> ForPoco<T>(IBuilder builder) where T : class, new()
         {
-            Type repoType;
-            if (!RepoCache.TryGetValue(typeof(T), out repoType))
-                repoType = RepoCache[typeof(T)] = typeof(PocoRepository<>).MakeGenericType(typeof(T));
+            var repoType = RepoCache.GetOrAdd(typeof(T), (key) => typeof(PocoRepository<>).MakeGenericType(typeof(T)));
 
             return (IPocoRepository<T>)Activator.CreateInstance(repoType, builder);
         }
         public IPocoRepository<TParent, T> ForPoco<TParent, T>(TParent parent, IBuilder builder) where T : class, new() where TParent : Entity<TParent>
         {
-            Type repoType;
-            if (!RepoCache.TryGetValue(typeof(T), out repoType))
-                repoType = RepoCache[typeof(T)] = typeof(PocoRepository<,>).MakeGenericType(typeof(TParent), typeof(T));
+            var repoType = RepoCache.GetOrAdd(typeof(T), (key) => typeof(PocoRepository<,>).MakeGenericType(typeof(TParent), typeof(T)));
 
             return (IPocoRepository<TParent, T>)Activator.CreateInstance(repoType, parent, builder);
         }
