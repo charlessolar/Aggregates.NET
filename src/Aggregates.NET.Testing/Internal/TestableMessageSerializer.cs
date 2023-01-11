b7b6d9cde7cc89d15d08b6195736f6a8c08cd93a
Charles Solar <charlessolar@gmail.com>
2023-01-10 22:07:33 -0600

    tweak serializer to keep inmemory serializations

--- src/Aggregates.NET.Testing/Internal/TestableMessageSerializer.cs
+++ src/Aggregates.NET.Testing/Internal/TestableMessageSerializer.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Aggregates.Internal
 {
@@ -9,13 +10,16 @@ namespace Aggregates.Internal
     {
         public string ContentType => "testing";
 
+        private static Dictionary<Type, object> Serialized = new Dictionary<Type, object>();
+
         public object[] Deserialize(Stream stream, IList<Type> messageTypes = null)
         {
-            return new object[] { };
+            return messageTypes.Where(x => Serialized.ContainsKey(x)).Select(x => Serialized[x]).ToArray();
         }
 
         public void Serialize(object message, Stream stream)
         {
+            Serialized[message.GetType()] = message;
         }
     }
 }
